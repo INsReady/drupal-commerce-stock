8667b7978e122ca287578dfebe2c167c424c75fb
Jingsheng Wang <skyred@insready.com>
2016-02-18 17:05:26 +0800

    Override the inline form to display quantity.

--- src/Form/StockInlineForm.php
+++ src/Form/StockInlineForm.php
@@ -7,4 +7,18 @@ use Drupal\inline_entity_form\Form\EntityInlineForm;
 /**
  * Defines the inline form for product stock.
  */
-class StockInlineForm extends EntityInlineForm {}
+class StockInlineForm extends EntityInlineForm {
+  /**
+   * {@inheritdoc}
+   */
+  public function tableFields($bundles) {
+    $fields = parent::tableFields($bundles);
+    $fields['quantity'] = [
+      'type' => 'field',
+      'label' => t('Quantity'),
+      'weight' => 200,
+    ];
+
+    return $fields;
+  }
+}
