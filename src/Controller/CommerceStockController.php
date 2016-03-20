3d307de724e20018451d7095349eb066748c4988
Jingsheng Wang <skyred@insready.com>
2016-03-21 00:33:47 +0800

    refactoring sku autocomplete.

--- src/Controller/CommerceStockController.php
+++ src/Controller/CommerceStockController.php
@@ -3,34 +3,19 @@
 namespace Drupal\commerce_stock\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Database\Connection;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class CommerceStockController extends ControllerBase {
 
-  protected $connection;
-
-  public function __construct(Connection $connection) {
-    $this->connection = $connection;
-  }
-
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('database')
-    );
-  }
-
   public function handleAutocomplete(Request $request) {
-    $matches = array();
 
     if ($input = $request->query->get('q')) {
-      $query = $this->connection->select('commerce_product_variation_field_data', 'cpv');
-      $query->fields('cpv', ['sku']);
-      $query->condition('cpv.sku', '%' . $input . '%', 'LIKE');
-      $query->range(0, 20);
-      $result = $query->execute()->fetchCol();
+      $result = \Drupal::entityQuery('commerce_product_variation')
+        ->condition('sku', $input, 'LIKE %...%')
+        ->condition('status', 1)
+        ->range(0, 10)
+        ->execute()->fetchCol();
     }
 
     return new JsonResponse($result);
